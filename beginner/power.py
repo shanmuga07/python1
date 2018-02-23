a3516566fd16936f198cf13bc31508e224c9e49f
shanmuga07 <36722947+shanmuga07@users.noreply.github.com>
2018-02-23 12:26:42 +0530

    Update power.py

--- beginner/power.py
+++ beginner/power.py
@@ -1,8 +1,4 @@
-def power(base,exp):
-    if(exp==1):
-        return(base)
-    if(exp!=1):
-        return(base*power(base,exp-1))
-base=int(input("Enter base: "))
-exp=int(input("Enter exponential value: "))
-print("Rsult:",power(base,exp))
+s=int(raw_input())
+t=int(raw_input())
+v=(s**t)
+print(v)
