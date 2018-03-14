6ba67cc6e69009443dc80f0fa06a9b9ad7bcbdb4
shanmuga07 <36722947+shanmuga07@users.noreply.github.com>
2018-03-14 16:25:21 +0530

    Create num.py

---
+++ beginner/num.py
@@ -0,0 +1,13 @@
+
+def sum( a, b,c) :
+    sum = 0
+    i = 0
+    while i < n :
+        sum = sum + a
+        a = a + c
+        i = i + 1
+    return sum
+c = 20
+a = 2.5
+b = 1.5
+print (sum(a, b, c)
